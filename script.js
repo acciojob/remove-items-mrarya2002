81ec4fb55b5df5590b2ba42fce2b53abfc3a2b41
Nikhil Arya <nikhilarya3108202@gmail.com>
2025-08-28 21:27:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let select = document.querySelector("#colorSelect");
+const button  = document.querySelector("input[type='button']");
+
+button.addEventListener("click",()=>{
+	let curr = select.value;
+	Array.from(select.options).forEach(op => {
+		  if (op.value === curr) {
+		    op.remove();
+	  }
+});
+})
